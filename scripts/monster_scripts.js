a3a0881dc598763062cc4ace05668eb2fbd8c7ef
TiGrant <112642677+TiGrant785@users.noreply.github.com>
2022-12-07 21:14:53 -0500

    Add files via upload

---
+++ scripts/monster_scripts.js
@@ -0,0 +1,120 @@
+'use strict';
+let clickHappened = false;
+
+$(function() {
+    let headclix = 0, 
+        eyeclix = 0, 
+        noseclix = 0, 
+        mouthclix = 0;
+
+        lightning_one();
+        lightning_two();
+        lightning_three();
+
+        $("#head").on("click", function() {
+            if (headclix < 9) {
+                $(this).animate({ left: "-=367px" }, 500);
+                headclix += 1;
+            } else if (headclix === 9) {
+                $(this).animate({ left: "0px" }, 500);
+                headclix = 0;
+            }
+            clickHappened = true;
+        });
+
+        $("#eyes").on("click", function() {
+            if (eyeclix < 9) {
+                $(this).animate({ left: "-=367px" }, 500);
+                eyeclix += 1;
+            } else {
+                $(this).animate({ left: "0px" }, 500);
+                eyeclix = 0;
+            }
+            clickHappened = true;
+        });
+
+        $("#nose").on("click", function() {
+            if (noseclix < 9) {
+                $(this).animate({ left: "-=367px" }, 500);
+                noseclix += 1;
+            } else {
+                $(this).animate({ left: "0px" }, 500);
+                noseclix = 0;
+            }
+            clickHappened = true;
+        });
+
+        $("#mouth").on("click", function() {
+            if (mouthclix < 9) {
+                $(this).animate({ left: "-=367px" }, 500);
+                mouthclix += 1;
+            } else {
+                $(this).animate({ left: "0px" }, 500);
+                mouthclix = 0;
+            }
+            clickHappened = true;
+        });
+
+        $('#head').on('contextmenu', function(e) {
+            e.preventDefault();
+            if (headclix > 0) {
+                $(this).animate({ left: "+=367px" }, 500);
+                headclix -= 1;
+            } 
+            return false;
+        });
+
+        $('#eyes').on('contextmenu', function(e) {
+            e.preventDefault();
+            if (eyeclix > 0) {
+                $(this).animate({ left: "+=367px" }, 500);
+                eyeclix -= 1;
+            } 
+            return false;
+        });
+
+        $('#nose').on('contextmenu', function(e) {
+            e.preventDefault();
+            if (noseclix > 0) {
+                $(this).animate({ left: "+=367px" }, 500);
+                noseclix -= 1;
+            } 
+            return false;
+        });
+
+        $('#mouth').on('contextmenu', function(e) {
+            e.preventDefault();
+            if (mouthclix > 0) {
+                $(this).animate({ left: "+=367px" }, 500);
+                mouthclix -= 1;
+            } 
+            else if (mouthclix === 9) {
+                 $(this).animate({ left: "0px"}, 500);
+                 mouthclix = 0;                
+            }
+        });
+});
+
+function lightning_one() {
+    $("#lightning1").fadeIn(250).fadeOut(250);
+    setTimeout("lightning_one()", 4000)
+};
+
+function lightning_two() {
+    $("#lightning2").fadeIn(250).fadeOut(250);
+    setTimeout("lightning_two()", 5000)
+};
+
+function lightning_three() {
+    $("#lightning3").fadeIn(250).fadeOut(250);
+    setTimeout("lightning_three()", 7000)
+    if (clickHappened)
+    playThunder();
+};
+
+function playThunder() {
+    console.log("playThunder");
+    $("#thunder").get(0).playbackRate = 3;
+    $("#thunder").get(0).play();
+    $("#thunder").get(0).muted = false;
+};
